4ec6bf0636176fa7421a5f281235bfbc554015b6
dcmox <dancmox@comcast.net>
2020-01-12 13:31:47 -0600

    better precision

--- src/lib.rs
+++ src/lib.rs
@@ -9,22 +9,22 @@ pub trait Timer {
 
 impl Testmark {
     pub fn start(&mut self) -> u128 {
-        self.start_time = time::UNIX_EPOCH.elapsed().unwrap().as_millis();
+        self.start_time = time::UNIX_EPOCH.elapsed().unwrap().as_micros();
         self.start_time
     }
     pub fn stop(&mut self) -> u128 {
-        self.end_time = time::UNIX_EPOCH.elapsed().unwrap().as_millis();
+        self.end_time = time::UNIX_EPOCH.elapsed().unwrap().as_micros();
         self.end_time
     }  
     pub fn time(&mut self) -> u128 {
         if self.end_time == 0 {
-            time::UNIX_EPOCH.elapsed().unwrap().as_millis() - self.start_time
+            time::UNIX_EPOCH.elapsed().unwrap().as_micros() - self.start_time
         } else {
             self.end_time - self.start_time
         }
     }
     pub fn print(&mut self) {
-        println!("{} took {} ms", self.name(), self.time())
+        println!("{0} took {1:.3} ms", self.name(), self.time() as f32 / 1000.00)
     }
 }
 
